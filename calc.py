e1bfc24649cc11a2fac873d8ab9b28924908bf0c
sreepriya-ak <25ad367@mgits.ac.in>
2026-09-21 00:00:16 +0530

    CALCULATOR CODE

---
+++ calc.py
@@ -0,0 +1,18 @@
+a = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+b = float(input("Enter second number: "))
+
+if op == '+':
+    print("Result =", a + b)
+
+elif op == '-':
+    print("Result =", a - b)
+
+elif op == '*':
+    print("Result =", a * b)
+
+elif op == '/':
+    if b != 0:
+        print("Result =", a / b)
+    else:
+        print("Cannot d
